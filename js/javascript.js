cc6f5a2b7190102e0ba11efe3449395203021985
Oliver <oliverkmiec24@gmail.com>
2020-08-24 19:14:32 -0400

    Update

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,8 @@
-let map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
-}
+setTimeout(() => {
+    let overlay = document.querySelector('.overlay');
+    for (let top = 0; top <= 120; top++) {
+        setTimeout(() => {
+            overlay.style.top = `-${top}%`
+        }, top * 10)
+    }
+}, 5000)
